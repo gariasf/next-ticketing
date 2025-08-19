0505fb079f12f21a091c72158dd63629eae1d3ae
Guillem Arias <hello@gariasf.com>
2025-08-19 17:25:08 +0200

    chore: add silly icons for statuses and bandaid const assertion to make ts happy for now

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,11 +2,20 @@ import Link from "next/link";
 import { initialTickts } from "@/data";
 import { ticketPathFor } from "@/paths";
 
+const TICKET_ICONS = {
+  OPEN: "O",
+  IN_PROGRESS: ">",
+  DONE: "X"
+}
+
 export default function TicketsPage() {
   return (
     <div>
       {initialTickts.map((ticket) => (
         <div key={ticket.id}>
+          <div>
+            {TICKET_ICONS[ticket.status]}
+          </div>
           <h2>{ticket.title}</h2>
 
           <Link href={ticketPathFor(ticket.id)} className="text-sm underline">
