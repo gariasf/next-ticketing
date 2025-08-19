cfba9062fe96317117df71d25791357dd3375b04
Guillem Arias <hello@gariasf.com>
2025-08-19 17:43:47 +0200

    chore: add clsx and super basic styling with tailwind

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -10,19 +10,25 @@ const TICKET_ICONS = {
 
 export default function TicketsPage() {
   return (
-    <div>
-      {initialTickts.map((ticket) => (
-        <div key={ticket.id}>
-          <div>
-            {TICKET_ICONS[ticket.status]}
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div className="flex flex-col gap-y-8">
+        <h2 className="text-3xl font-bold tracking-tight">TicketsPage</h2>
+        <p className="text-sm text-muted-foreground">All your tickets in one place</p>
+      </div>
+      <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
+        {initialTickts.map((ticket) => (
+          <div key={ticket.id} className="w-full max-w-[420px] p-4 border border-slate-100 rounded">
+            <div>
+              {TICKET_ICONS[ticket.status]}
+            </div>
+            <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
+            <p className="text-sm text-slate-400 truncate">{ticket.content}</p>
+            <Link href={ticketPathFor(ticket.id)} className="text-sm underline">
+              View
+            </Link>
           </div>
-          <h2>{ticket.title}</h2>
-
-          <Link href={ticketPathFor(ticket.id)} className="text-sm underline">
-            View
-          </Link>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
