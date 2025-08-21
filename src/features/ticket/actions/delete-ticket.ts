6ef8c3cf77e99e63d8be36ac72da1c1ccfc5dc4c
Guillem Arias <hello@gariasf.com>
2025-08-21 16:03:41 +0200

    chore: reorder import

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -1,6 +1,6 @@
 'use server';
+import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-import {revalidatePath} from 'next/cache'
 import { prisma } from '@/lib/prisma';
 import { ticketsPath } from '@/paths';
 
