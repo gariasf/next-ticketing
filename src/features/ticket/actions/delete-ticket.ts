0781451ce1888bf048c90ffdc7c2ed177638364d
Guillem Arias <hello@gariasf.com>
2025-08-23 11:27:00 +0200

    feat: set toast cookies

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -1,6 +1,7 @@
 'use server';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { setCookieByKey } from '@/actions/cookies';
 import { prisma } from '@/lib/prisma';
 import { ticketsPath } from '@/paths';
 
@@ -12,5 +13,6 @@ export async function deleteTicket(id: string) {
   });
 
   revalidatePath(ticketsPath());
+  setCookieByKey('toast', 'Ticket deleted');
   redirect(ticketsPath());
 }
