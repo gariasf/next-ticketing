250d861ef7f2266f27162f8cad2d58787d6ad238
Guillem Arias <hello@gariasf.com>
2025-08-24 18:34:56 +0200

    feat: add lucia init file

--- src/paths.ts
+++ src/paths.ts
@@ -4,7 +4,6 @@ export const ticketsPath = () => '/tickets';
 export const ticketPathFor = (id: string) => `${ticketsPath()}/${id}`;
 export const ticketEditPathFor = (id: string) => `${ticketPathFor(id)}/edit`;
 
-
 export const signUpPath = () => '/sign-up';
 export const signInPath = () => '/sign-in';
 export const passwordResetPath = () => '/password-reset';
