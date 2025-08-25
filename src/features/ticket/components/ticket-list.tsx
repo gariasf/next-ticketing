4f8d9cfabdd81ed04408a709ecc1e8a72e17aecd
Guillem Arias <hello@gariasf.com>
2025-08-25 18:28:43 +0200

    feat: sort tickets

--- src/features/ticket/components/ticket-list.tsx
+++ src/features/ticket/components/ticket-list.tsx
@@ -1,5 +1,6 @@
 import { Placeholder } from '@/components/placeholder';
 import { SearchInput } from '@/components/search-input';
+import { SortSelect } from '@/components/sort-select';
 import { getTickets } from '../queries/get-tickets';
 import { SearchParams } from '../search-params';
 import { TicketItem } from './ticket-item';
@@ -14,8 +15,15 @@ export async function TicketList({ userId, searchParams }: TicketListProps) {
 
   return (
     <div className="flex-1 flex flex-col items-center gap-4">
-      <div className="w-full max-w-[420px]">
-        <SearchInput placeholder="Search tickets..." />
+      <div className="w-full max-w-[420px] flex gap-x-2">
+        <SearchInput placeholder="Search tickets ..." />
+        <SortSelect
+          defaultValue="newest"
+          options={[
+            { value: 'newest', label: 'Newest' },
+            { value: 'bounty', label: 'Bounty' },
+          ]}
+        />
       </div>
       {tickets.length ? (
         tickets.map((ticket) => <TicketItem key={ticket.id} ticket={ticket} />)
