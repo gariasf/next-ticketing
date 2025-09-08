e853d6c13e5ce03c88575dc58e88a2a4ac819195
Guillem Arias <hello@gariasf.com>
2025-09-08 11:19:32 +0200

    feat: use nuqs to type search params

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import { ThemeProvider } from '@/components/theme/theme-provider';
 import { Toaster } from '@/components/ui/sonner';
 import { Header } from './_navigation/header';
@@ -31,24 +32,26 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider>
-          <Header />
-          <div className="flex h-screen overflow-hidden border-collapse">
-            <Sidebar />
-            <main
-              className="
+        <NuqsAdapter>
+          <ThemeProvider>
+            <Header />
+            <div className="flex h-screen overflow-hidden border-collapse">
+              <Sidebar />
+              <main
+                className="
                          min-h-screen flex-1
                          overflow-y-auto overflow-x-hidden
                          py-24 px-8
                          bg-secondary/20
                          flex flex-col
                        "
-            >
-              {children}
-            </main>
-          </div>
-          <Toaster expand />
-        </ThemeProvider>
+              >
+                {children}
+              </main>
+            </div>
+            <Toaster expand />
+          </ThemeProvider>
+        </NuqsAdapter>
       </body>
     </html>
   );
--- src/features/ticket/search-params.ts
+++ src/features/ticket/search-params.ts
@@ -1,4 +1,10 @@
-export type SearchParams = {
-  search?: string;
-  sort?: string | string[];
-};
+import { createSearchParamsCache, parseAsString } from 'nuqs/server';
+
+export const searchParamsCache = createSearchParamsCache({
+  search: parseAsString.withDefault(''),
+  sort: parseAsString.withDefault('newest'),
+});
+
+export type ParsedSearchParams = Awaited<
+  ReturnType<typeof searchParamsCache.parse>
+>;
