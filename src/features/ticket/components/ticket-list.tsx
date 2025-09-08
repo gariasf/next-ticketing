a4210efaa36cf9e45a58d6a210c0a708cb466d37
Guillem Arias <hello@gariasf.com>
2025-09-08 12:19:06 +0200

    feat: basic pagination

--- src/features/ticket/components/ticket-list.tsx
+++ src/features/ticket/components/ticket-list.tsx
@@ -2,6 +2,7 @@ import { Placeholder } from '@/components/placeholder';
 import { getTickets } from '../queries/get-tickets';
 import { ParsedSearchParams } from '../search-params';
 import { TicketItem } from './ticket-item';
+import { TicketPagination } from './ticket-pagination';
 import { TicketSearchInput } from './ticket-search-input';
 import { TicketSortSelect } from './ticket-sort-select';
 
@@ -42,6 +43,9 @@ export async function TicketList({ userId, searchParams }: TicketListProps) {
       ) : (
         <Placeholder label="No tickets found" />
       )}
+      <div className="w-full max-w-[420px]">
+        <TicketPagination />
+      </div>
     </div>
   );
 }
