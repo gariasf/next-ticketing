18ef275d2826125d87bc4665777d75d3ac4147d0
Guillem Arias <hello@gariasf.com>
2025-09-08 12:35:50 +0200

    fix: reset pagination reactively

--- src/features/ticket/components/ticket-pagination.tsx
+++ src/features/ticket/components/ticket-pagination.tsx
@@ -1,8 +1,13 @@
 'use client';
 
-import { useQueryStates } from 'nuqs';
+import { useQueryState, useQueryStates } from 'nuqs';
+import { useEffect, useRef } from 'react';
 import { Pagination } from '@/components/pagination';
-import { paginationOptions, paginationParser } from '../search-params';
+import {
+  paginationOptions,
+  paginationParser,
+  searchParser,
+} from '../search-params';
 
 type TicketPaginationProps = {
   paginatedTicketMetadata: {
@@ -18,6 +23,15 @@ export function TicketPagination({
     paginationParser,
     paginationOptions
   );
+  const [search] = useQueryState('search', searchParser);
+  const prevSearch = useRef(search);
+
+  useEffect(() => {
+    if (search === prevSearch.current) return;
+    prevSearch.current = search;
+
+    setPagination({ ...pagination, page: 0 });
+  }, [search, pagination, setPagination]);
 
   return (
     <Pagination
