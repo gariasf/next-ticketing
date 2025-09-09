75c81c0c37558e3c4930768f64324aff0aa4136d
Guillem Arias <hello@gariasf.com>
2025-09-09 22:30:48 +0200

    fix: ensure that comment formatting is consistent in server and browser sides

--- src/features/comment/components/comment-item.tsx
+++ src/features/comment/components/comment-item.tsx
@@ -1,3 +1,4 @@
+import { format } from 'date-fns';
 import { Card } from '@/components/ui/card';
 import { CommentWithMetadata } from '../types';
 
@@ -17,7 +18,7 @@ export function CommentItem({ comment, buttons }: CommentItemProps) {
               : (comment.user?.username ?? 'Deleted User')}
           </p>
           <p className="text-sm text-muted-foreground">
-            {comment.createdAt.toLocaleString()}
+            {format(comment.createdAt, 'yyyy-MM-dd, HH:mm')}
           </p>
         </div>
         <p className="whitespace-pre-line">{comment.content}</p>
