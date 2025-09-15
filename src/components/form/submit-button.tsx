187f8cfad9b48080975d40b40308f0f556d9e9bc
Guillem Arias <hello@gariasf.com>
2025-09-15 11:33:31 +0200

    chore: simplify SubmitButton component and types

--- src/components/form/submit-button.tsx
+++ src/components/form/submit-button.tsx
@@ -1,21 +1,14 @@
 'use client';
 
-import clsx from 'clsx';
 import { LucideLoaderCircle } from 'lucide-react';
 import { useFormStatus } from 'react-dom';
-import { Button } from '../ui/button';
+import { Button, ButtonProps } from '../ui/button';
 
 type SubmitButtonProps = {
   label: string;
   icon?: React.ReactElement;
-  variant?:
-    | 'default'
-    | 'destructive'
-    | 'outline'
-    | 'secondary'
-    | 'ghost'
-    | 'link';
-  size?: 'default' | 'sm' | 'lg' | 'icon';
+  variant?: ButtonProps['variant'];
+  size?: ButtonProps['size'];
 };
 
 export function SubmitButton({
@@ -28,15 +21,12 @@ export function SubmitButton({
 
   return (
     <Button disabled={pending} type="submit" variant={variant} size={size}>
-      {pending && (
-        <LucideLoaderCircle
-          className={clsx('animate-spin', {
-            'mr-2': !!label,
-          })}
-        />
-      )}
+      {pending ? (
+        <LucideLoaderCircle className="h-4 w-4 animate-spin" />
+      ) : icon ? (
+        <>{icon}</>
+      ) : null}
       {label}
-      {pending ? null : icon ? <span>{icon}</span> : null}
     </Button>
   );
 }
