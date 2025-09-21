5ce5b70f63d90564bab8927ffeea880a35572833
Guillem Arias <hello@gariasf.com>
2025-09-21 13:06:59 +0200

    refactor: remove revalidation from organization deletion action and refresh UI on success

--- src/features/organization/components/organization-delete-button.tsx
+++ src/features/organization/components/organization-delete-button.tsx
@@ -3,6 +3,7 @@
 import { LucideLoaderCircle, LucideTrash } from 'lucide-react';
 import { useConfirmDialog } from '@/components/confirm-dialog';
 import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
 import { deleteOrganization } from '../actions/delete-organization';
 
 type OrganizationDeleteButtonProps = {
@@ -12,8 +13,12 @@ type OrganizationDeleteButtonProps = {
 export function OrganizationDeleteButton({
   organizationId,
 }: OrganizationDeleteButtonProps) {
+  const router = useRouter();
   const [deleteButton, deleteDialog] = useConfirmDialog({
     action: deleteOrganization.bind(null, organizationId),
+    onSuccess: () => {
+      router.refresh();
+    },
     trigger: (isPending) => (
       <Button variant="destructive" size="icon">
         {isPending ? (
