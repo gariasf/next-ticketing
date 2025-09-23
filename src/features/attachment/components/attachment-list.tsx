066ad6f2303a346c2945ab8cabfa93adb5bb238f
Guillem Arias <hello@gariasf.com>
2025-09-23 17:51:01 +0200

    chore: extract components and hooks to improve code organization

---
+++ src/features/attachment/components/attachment-list.tsx
@@ -0,0 +1,21 @@
+import { Attachment } from '@prisma/client';
+import { AttachmentItem } from './attachment-item';
+
+type AttachmentListProps = {
+  attachments: Attachment[];
+  buttons: (id: string) => React.ReactNode[];
+};
+
+export function AttachmentList({ attachments, buttons }: AttachmentListProps) {
+  return (
+    <div className="mx-2 flex flex-col gap-y-2 mb-4">
+      {attachments.map((attachment) => (
+        <AttachmentItem
+          key={attachment.id}
+          attachment={attachment}
+          buttons={buttons(attachment.id)}
+        />
+      ))}
+    </div>
+  );
+}
---
+++ src/features/comment/components/comment-list.tsx
@@ -0,0 +1,30 @@
+import { CommentWithMetadata } from '../types';
+import { CommentDeleteButton } from './comment-delete-button';
+import { CommentItem } from './comment-item';
+
+type CommentListProps = {
+  comments: CommentWithMetadata[];
+  onDeleteComment: (id: string) => void;
+};
+
+export function CommentList({ comments, onDeleteComment }: CommentListProps) {
+  return (
+    <>
+      {comments.map((comment) => {
+        const commentDeleteButton = (
+          <CommentDeleteButton
+            key="0"
+            id={comment.id}
+            onDeleteComment={onDeleteComment}
+          />
+        );
+
+        const buttons = [...(comment.isOwner ? [commentDeleteButton] : [])];
+
+        return (
+          <CommentItem key={comment.id} comment={comment} buttons={buttons} />
+        );
+      })}
+    </>
+  );
+}
